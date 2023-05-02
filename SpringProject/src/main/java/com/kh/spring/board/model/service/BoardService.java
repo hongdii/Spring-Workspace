716ae3d8837fe1e44da889423e9a7cc4635d1c5a
yejin0220 <dhguswl00@naver.com>
2023-05-02 15:17:46 +0900

    0502

--- SpringProject/src/main/java/com/kh/spring/board/model/service/BoardService.java
+++ SpringProject/src/main/java/com/kh/spring/board/model/service/BoardService.java
@@ -2,7 +2,10 @@ package com.kh.spring.board.model.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.spring.board.model.vo.Board;
 import com.kh.spring.board.model.vo.BoardType;
@@ -11,10 +14,15 @@ public interface BoardService {
 	
 	public ArrayList<BoardType> selectBoardTypeList();
 	
-	public void selectBoardList(int currentPage, String boardCode , Map<String, Object> map);
+	public void selectBoardList(int currentPage, Map<String, Object> map);
 
 	public Board selectBoardDetail(int boardNo);
 	
 	public int updateReadCount(int boardNo);
 	
+	public void selectBoardKeywordList(int currentPage, Map<String, Object> map);
+	
+	public int insertBoard(Board b, List<MultipartFile> list, String webPath, String serverFolderPath) throws Exception;
+
+	public int updateBoard(Board b, List<MultipartFile> list, String webPath, String serverFolderPath, String deleteList) throws Exception;
 }
